7d8327958e5fed806ed486a18fff3c2eedcc35f9
GowthamS2021 <msridharn@outlook.com>
2023-04-26 23:29:16 +0530

    intial draft

---
+++ ChampSim/run_hashed.sh
@@ -0,0 +1,9 @@
+
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_1.xz
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_2.xz
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_3.xz
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_4.xz
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_5.xz
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_6.xz
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_7.xz
+./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 30 30 trace_8.xz
